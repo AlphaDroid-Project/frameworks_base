fe4ba4bf41fc77e434e14cbd38692a44cf3f398e
Pranav Vashi <neobuddy89@gmail.com>
2018-09-28 03:21:56 +0530

    Set summary automatically if required
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/LineagePreferenceLib/java/lineageos/preference/LineageSecureSettingListPreference.java
+++ packages/LineagePreferenceLib/java/lineageos/preference/LineageSecureSettingListPreference.java
@@ -6,11 +6,14 @@
 package lineageos.preference;
 
 import android.content.Context;
+import android.text.TextUtils;
 import android.util.AttributeSet;
 
 import android.provider.Settings;
 
 public class LineageSecureSettingListPreference extends SelfRemovingListPreference {
+
+    private boolean mAutoSummary = false;
 
     public LineageSecureSettingListPreference(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
@@ -18,6 +21,24 @@ public class LineageSecureSettingListPreference extends SelfRemovingListPreferen
 
     public LineageSecureSettingListPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
+    }
+
+    @Override
+    public void setValue(String value) {
+        super.setValue(value);
+        if (mAutoSummary || TextUtils.isEmpty(getSummary())) {
+            setSummary(getEntry(), true);
+        }
+    }
+
+    @Override
+    public void setSummary(CharSequence summary) {
+        setSummary(summary, false);
+    }
+
+    private void setSummary(CharSequence summary, boolean autoSummary) {
+        mAutoSummary = autoSummary;
+        super.setSummary(summary);
     }
 
     public int getIntValue(int defValue) {
--- packages/LineagePreferenceLib/java/lineageos/preference/LineageSystemSettingListPreference.java
+++ packages/LineagePreferenceLib/java/lineageos/preference/LineageSystemSettingListPreference.java
@@ -6,17 +6,39 @@
 package lineageos.preference;
 
 import android.content.Context;
+import android.text.TextUtils;
 import android.util.AttributeSet;
 
 import android.provider.Settings;
 
 public class LineageSystemSettingListPreference extends SelfRemovingListPreference {
+
+    private boolean mAutoSummary = false;
+
     public LineageSystemSettingListPreference(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
     }
 
     public LineageSystemSettingListPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
+    }
+
+    @Override
+    public void setValue(String value) {
+        super.setValue(value);
+        if (mAutoSummary || TextUtils.isEmpty(getSummary())) {
+            setSummary(getEntry(), true);
+        }
+    }
+
+    @Override
+    public void setSummary(CharSequence summary) {
+        setSummary(summary, false);
+    }
+
+    private void setSummary(CharSequence summary, boolean autoSummary) {
+        mAutoSummary = autoSummary;
+        super.setSummary(summary);
     }
 
     public int getIntValue(int defValue) {
