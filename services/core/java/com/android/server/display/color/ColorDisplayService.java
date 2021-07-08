4beb88135900b99ed0e09195013507e0ac982cc9
Danny Lin <danny@kdrag0n.dev>
2021-07-08 03:34:50 -0700

    display: Add simple RGB color balance transform
    This adds support for adjusting the display's RGB color balance
    globally via ColorDisplayManager. This is implemented as a simple
    color transformation matrix that scales each channel.
    
    The effect is similar to LineageOS' LiveDisplay feature, but being a
    native color transform, it doesn't require changes to native code,
    SELinux policies, or custom HALs.
    
    Change-Id: Ia7ce363e3042ecfae438e452dbf155811a6bedeb
    Signed-off-by: Leonardo Ledda <leonardoledda@gmail.com>

--- services/core/java/com/android/server/display/color/ColorDisplayService.java
+++ services/core/java/com/android/server/display/color/ColorDisplayService.java
@@ -125,6 +125,7 @@ public final class ColorDisplayService extends SystemService {
     private static final int MSG_APPLY_GLOBAL_SATURATION = 4;
     private static final int MSG_APPLY_DISPLAY_WHITE_BALANCE = 5;
     private static final int MSG_APPLY_REDUCE_BRIGHT_COLORS = 6;
+    private static final int MSG_APPLY_DISPLAY_COLOR_BALANCE = 7;
 
     /**
      * Return value if a setting has not been set.
@@ -166,6 +167,8 @@ public final class ColorDisplayService extends SystemService {
                     LocalServices.getService(DisplayManagerInternal.class), mDisplayManagerFlags);
     private final NightDisplayTintController mNightDisplayTintController =
             new NightDisplayTintController();
+    private final ColorBalanceTintController mColorBalanceTintController =
+            new ColorBalanceTintController();
     private final TintController mGlobalSaturationTintController =
             new GlobalSaturationTintController();
     private final ReduceBrightColorsTintController mReduceBrightColorsTintController =
@@ -378,6 +381,11 @@ public final class ColorDisplayService extends SystemService {
                                     onAccessibilityDaltonizerChanged();
                                 }
                                 break;
+                            case Secure.DISPLAY_COLOR_BALANCE_RED:
+                            case Secure.DISPLAY_COLOR_BALANCE_BLUE:
+                            case Secure.DISPLAY_COLOR_BALANCE_GREEN:
+                                mHandler.sendEmptyMessage(MSG_APPLY_DISPLAY_COLOR_BALANCE);
+                                break;
                             case Secure.DISPLAY_WHITE_BALANCE_ENABLED:
                                 updateDisplayWhiteBalanceStatus();
                                 break;
@@ -413,6 +421,12 @@ public final class ColorDisplayService extends SystemService {
                 Secure.getUriFor(Secure.ACCESSIBILITY_DISPLAY_DALTONIZER_ENABLED),
                 false /* notifyForDescendants */, mContentObserver, mCurrentUser);
         cr.registerContentObserver(Secure.getUriFor(Secure.ACCESSIBILITY_DISPLAY_DALTONIZER),
+                false /* notifyForDescendants */, mContentObserver, mCurrentUser);
+        cr.registerContentObserver(Secure.getUriFor(Secure.DISPLAY_COLOR_BALANCE_RED),
+                false /* notifyForDescendants */, mContentObserver, mCurrentUser);
+        cr.registerContentObserver(Secure.getUriFor(Secure.DISPLAY_COLOR_BALANCE_GREEN),
+                false /* notifyForDescendants */, mContentObserver, mCurrentUser);
+        cr.registerContentObserver(Secure.getUriFor(Secure.DISPLAY_COLOR_BALANCE_BLUE),
                 false /* notifyForDescendants */, mContentObserver, mCurrentUser);
         cr.registerContentObserver(Secure.getUriFor(Secure.DISPLAY_WHITE_BALANCE_ENABLED),
                 false /* notifyForDescendants */, mContentObserver, mCurrentUser);
@@ -471,6 +485,10 @@ public final class ColorDisplayService extends SystemService {
                 onReduceBrightColorsActivationChanged(/*userInitiated*/ false);
                 mHandler.sendEmptyMessage(MSG_APPLY_REDUCE_BRIGHT_COLORS);
             }
+        }
+
+        if (mColorBalanceTintController.isAvailable(getContext())) {
+            mHandler.sendEmptyMessage(MSG_APPLY_DISPLAY_COLOR_BALANCE);
         }
     }
 
@@ -1123,6 +1141,25 @@ public final class ColorDisplayService extends SystemService {
             }
         }
         return false;
+    }
+
+    private boolean setColorBalanceChannelInternal(int channel, int value) {
+        if (mCurrentUser == UserHandle.USER_NULL) {
+            return false;
+        }
+
+        boolean putSuccess = Secure.putIntForUser(getContext().getContentResolver(),
+                ColorBalanceTintController.channelToKey(channel), value, mCurrentUser);
+        if (putSuccess) {
+            mHandler.sendEmptyMessage(MSG_APPLY_DISPLAY_COLOR_BALANCE);
+        }
+
+        return putSuccess;
+    }
+
+    private int getColorBalanceChannelInternal(int channel) {
+        return Secure.getIntForUser(getContext().getContentResolver(),
+                ColorBalanceTintController.channelToKey(channel), 255, mCurrentUser);
     }
 
     private void dumpInternal(PrintWriter pw) {
@@ -1789,6 +1826,10 @@ public final class ColorDisplayService extends SystemService {
                 case MSG_APPLY_DISPLAY_WHITE_BALANCE:
                     applyTintByCct(mDisplayWhiteBalanceTintController, false);
                     break;
+                case MSG_APPLY_DISPLAY_COLOR_BALANCE:
+                    mColorBalanceTintController.updateBalance(getContext(), mCurrentUser);
+                    applyTint(mColorBalanceTintController, true);
+                    break;
             }
         }
     }
@@ -2048,6 +2089,26 @@ public final class ColorDisplayService extends SystemService {
         }
 
         @android.annotation.EnforcePermission(android.Manifest.permission.CONTROL_DISPLAY_COLOR_TRANSFORMS)
+        @Override
+        public boolean setColorBalanceChannel(int channel, int value) {
+            final long token = Binder.clearCallingIdentity();
+            try {
+                return setColorBalanceChannelInternal(channel, value);
+            } finally {
+                Binder.restoreCallingIdentity(token);
+            }
+        }
+
+        @Override
+        public int getColorBalanceChannel(int channel) {
+            final long token = Binder.clearCallingIdentity();
+            try {
+                return getColorBalanceChannelInternal(channel);
+            } finally {
+                Binder.restoreCallingIdentity(token);
+            }
+        }
+
         @Override
         public boolean setDisplayWhiteBalanceEnabled(boolean enabled) {
             setDisplayWhiteBalanceEnabled_enforcePermission();
