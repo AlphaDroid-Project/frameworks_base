8b91989ec1da53a1698e0d5b143c89f00ebe08a8
SagarMakhar <sagarmakhar@gmail.com>
2021-08-28 14:15:37 +0000

    base: Allow locking tasks to recents [1/2]
    Change-Id: Id18af94cf52db46054299a862b91c739e8d204f6
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>
    Signed-off-by: mukesh22584 <mks22584@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7436,6 +7436,11 @@ public final class Settings {
          */
         public static final String STATUSBAR_NOTIF_COUNT = "statusbar_notif_count";
 
+         /**
+          * @hide
+          */
+         public static final String RECENTS_LOCKED_TASKS = "recents_locked_tasks";
+
         /**
          * Enable/disable Bluetooth Battery bar
          * @hide
