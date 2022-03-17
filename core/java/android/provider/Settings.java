e680d60719afb0c95d30c0aa7155c778fe721a1c
spkal01 <kalligeross@gmail.com>
2022-03-17 11:10:12 +0800

    Introduce Custom settings layout
    Change-Id: Ic32bc00804720dbe5d22c63c0a9c84e410afc645
    Signed-off-by: dlwlrma123 <alexfinhart@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6321,6 +6321,12 @@ public final class Settings {
         public static final String SETTINGS_DASHBOARD_STYLE = "settings_dashboard_style";
 
         /**
+         * Settings Dashboard Style
+         * @hide
+         */
+        public static final String SETTINGS_DASHBOARD_STYLE = "settings_dashboard_style";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
