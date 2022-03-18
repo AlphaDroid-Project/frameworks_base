016deb2a7358667d19cd50beec478b16a91da712
LibXZR <i@xzr.moe>
2022-03-18 10:12:37 +0800

    Base: Ignore window secure flags [1/2]
    Change-Id: Ie2e45f3aa4b5d40133cae321b4fd99554d771bff

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -12204,6 +12204,13 @@ public final class Settings {
         public static final String SHOW_MEDIA_SQUIGGLE_ANIMATION = "show_media_squiggle_animation";
 
         /**
+         * Control whether FLAG_SECURE is ignored for all windows.
+         * @hide
+         */
+        @Readable
+        public static final String WINDOW_IGNORE_SECURE = "window_ignore_secure";
+
+        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */
