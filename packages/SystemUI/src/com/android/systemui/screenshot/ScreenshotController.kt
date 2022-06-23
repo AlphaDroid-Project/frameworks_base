6c5611993a9bc94ddd2ba8cbeee8051981fd459e
Cosmin Tanislav <demonsingur@gmail.com>
2022-06-24 02:38:49 +0300

    SystemUI: screenshot: open long screenshot activity for partial screenshots
    The partial screenshot UI is basically unusable. Replace it
    with the long screenshot UI which offers cropping, saving,
    canceling, and sharing capabilities.
    
    Change-Id: I27f7a811838f7e3f2057f567c7a350255996a8cb

--- packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.kt
+++ packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.kt
@@ -50,6 +50,7 @@ import com.android.systemui.dagger.qualifiers.Main
 import com.android.systemui.res.R
 import com.android.systemui.screenshot.ActionIntentCreator.createLongScreenshotIntent
 import com.android.systemui.screenshot.ScreenshotShelfViewProxy.ScreenshotViewCallback
+import com.android.systemui.screenshot.scroll.ScrollCaptureController.BitmapScreenshot
 import com.android.systemui.screenshot.scroll.ScrollCaptureController.LongScreenshot
 import com.android.systemui.screenshot.scroll.ScrollCaptureExecutor
 import com.android.systemui.util.Assert
@@ -170,6 +171,12 @@ internal constructor(
             info.loadLabel(packageManager).toString()
         }.getOrDefault("")
         scrollCaptureExecutor.longScreenshotHolder.foregroundAppName = packageLabel
+
+        if (screenshot.type == WindowManager.TAKE_SCREENSHOT_SELECTED_REGION) {
+            startPartialScreenshotActivity(Process.myUserHandle())
+            finisher.accept(null)
+            return
+        }
 
         val currentBitmap = screenshot.bitmap
         if (currentBitmap == null) {
@@ -394,6 +401,19 @@ internal constructor(
                 onScrollButtonClicked(owner, response)
             }
         }
+    }
+
+    private fun startPartialScreenshotActivity(owner: UserHandle) {
+        scrollCaptureExecutor.executeBatchScrollCapture(
+            BitmapScreenshot(context, imageCapture.captureDisplay(display.displayId, null)),
+            {
+                val intent = createLongScreenshotIntent(owner, context)
+                context.startActivity(intent)
+            },
+            { _: Rect, onTransitionEnd: Runnable, _: LongScreenshot ->
+                onTransitionEnd.run()
+            },
+        )
     }
 
     private fun onScrollButtonClicked(owner: UserHandle, response: ScrollCaptureResponse) {
