2ad049215bfaa118c9a7f40520b1cc3515c8db46
Pranav Vashi <neobuddy89@gmail.com>
2022-07-13 21:05:54 +0530

    SystemUI: Update black theme package overlay
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayApplier.java
+++ packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayApplier.java
@@ -68,7 +68,7 @@ public class ThemeOverlayApplier implements Dumpable {
     static final String SYSUI_PACKAGE = "com.android.systemui";
 
     static final String OVERLAY_BLACK_THEME =
-            "org.lineageos.overlay.customization.blacktheme";
+            "com.bootleggers.shishufied.theme.black.android";
 
     static final String OVERLAY_CATEGORY_DYNAMIC_COLOR =
             "android.theme.customization.dynamic_color";
