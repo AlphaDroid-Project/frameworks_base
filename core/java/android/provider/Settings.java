b3b5fcde241fab8bdeb6a1bf5a3056247a7a0992
elpaablo <donopwhile1@gmail.com>
2022-08-14 03:41:17 +0100

    Statusbar: adjustable max visible notification icons [1/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -8277,6 +8277,12 @@ public final class Settings {
         public static final String QS_TILE_ICON_SHAPE = "qs_tile_icon_shape";
 
         /**
+         * Max visible notification icons in the Statusbar
+         * @hide
+         */
+        public static final String MAX_VISIBLE_NOTIFICATION_ICONS = "max_visible_notification_icons";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
