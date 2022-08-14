7754687f62927e1bebc33ad7f4e8def6d9229acf
elpaablo <donopwhile1@gmail.com>
2022-08-14 03:41:17 +0100

    Statusbar: adjustable max visible notification icons [1/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -8279,6 +8279,12 @@ public final class Settings {
         public static final String ONGOING_CHIP_COLOR_MODE = "ongoing_chip_color_mode";
 
         /**
+         * Max visible notification icons in the Statusbar
+         * @hide
+         */
+        public static final String MAX_VISIBLE_NOTIFICATION_ICONS = "max_visible_notification_icons";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
