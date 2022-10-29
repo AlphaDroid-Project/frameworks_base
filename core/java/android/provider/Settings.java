262a18724395ce44c9250bcd53c51cc17c23053a
minaripenguin37 <alexfinhart@gmail.com>
2022-10-29 15:02:12 +0800

    SystemUI: Volume styles [1/2]
    SystemUI: Implement Aqua design to volume panel
    
    * ui concept by AYAN98542
    
    Co-authored-by: Ayan <79016495+AYAN98542@users.noreply.github.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6332,6 +6332,12 @@ public final class Settings {
         public static final String SETTINGS_DASHBOARD_STYLE = "settings_dashboard_style";
 
         /**
+         * Volume styles
+         * @hide
+         */
+        public static final String CUSTOM_VOLUME_STYLES = "custom_volume_styles";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -79,6 +79,7 @@ import android.os.Looper;
 import android.os.Message;
 import android.os.SystemClock;
 import android.os.Trace;
+import android.os.UserHandle;
 import android.os.VibrationEffect;
 import android.provider.DeviceConfig;
 import android.provider.Settings;
@@ -176,6 +177,8 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
             "system:" + Settings.System.VOLUME_DIALOG_TIMEOUT;
     public static final String VOLUME_MEDIA_OUTPUT_TOGGLE =
             "system:" + Settings.System.VOLUME_MEDIA_OUTPUT_TOGGLE;
+    public static final String CUSTOM_VOLUME_STYLES =
+            "system:" + Settings.System.CUSTOM_VOLUME_STYLES;
 
     private static final long USER_ATTEMPT_GRACE_PERIOD = 1000;
     private static final int UPDATE_ANIMATION_DURATION = 80;
@@ -337,6 +340,8 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
     // Number of animating rows
     private int mAnimatingRows = 0;
 
+    private int customVolumeStyles = 0;
+
     private boolean mShowMediaController = true;
 
     @VisibleForTesting
@@ -408,6 +413,7 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
         }
         mTunerService.addTunable(mTunable, VOLUME_DIALOG_TIMEOUT);
         mTunerService.addTunable(mTunable, VOLUME_MEDIA_OUTPUT_TOGGLE);
+        mTunerService.addTunable(mTunable, CUSTOM_VOLUME_STYLES);
 
         initDimens();
 
@@ -809,7 +815,7 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
                 }
 
                 addRow(STREAM_ALARM,
-                        R.drawable.ic_alarm, R.drawable.ic_volume_alarm_mute, true, false);
+                        R.drawable.ic_volume_alarm, R.drawable.ic_volume_alarm_mute, true, false);
                 addRow(AudioManager.STREAM_VOICE_CALL,
                         com.android.internal.R.drawable.ic_phone,
                         com.android.internal.R.drawable.ic_phone, false, false);
@@ -882,6 +888,14 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
             } else if (VOLUME_MEDIA_OUTPUT_TOGGLE.equals(key)) {
                 mShowMediaController =  TunerService.parseIntegerSwitch(newValue, true);
                 initSettingsH(mActivityManager.getLockTaskModeState());
+            } else if (CUSTOM_VOLUME_STYLES.equals(key)) {
+                final int selectedVolStyle = TunerService.parseInteger(newValue, 0);
+                if (customVolumeStyles != selectedVolStyle) {
+                    customVolumeStyles = selectedVolStyle;
+                    mHandler.post(() -> {
+                        mControllerCallbackH.onConfigurationChanged();
+                    });
+                }
             }
         }
     };
@@ -1000,7 +1014,13 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
         row.iconMuteRes = iconMuteRes;
         row.important = important;
         row.defaultStream = defaultStream;
-        row.view = mDialog.getLayoutInflater().inflate(R.layout.volume_dialog_row, null);
+        if (customVolumeStyles == 0) {
+           row.view = mDialog.getLayoutInflater().inflate(R.layout.volume_dialog_row_aosp, null);
+        } else if (customVolumeStyles == 1) {
+           row.view = mDialog.getLayoutInflater().inflate(R.layout.volume_dialog_row_rui, null);
+        } else {
+           row.view = mDialog.getLayoutInflater().inflate(R.layout.volume_dialog_row_rice, null);
+        }
         row.view.setId(row.stream);
         row.view.setTag(row);
         row.header = row.view.findViewById(R.id.volume_row_header);
@@ -1015,8 +1035,18 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
 
         row.anim = null;
 
-        final LayerDrawable seekbarDrawable =
-                (LayerDrawable) mContext.getDrawable(R.drawable.volume_row_seekbar);
+        final LayerDrawable seekbarDrawable;
+
+        if (customVolumeStyles == 0) {
+              seekbarDrawable =
+                      (LayerDrawable) mContext.getDrawable(R.drawable.volume_row_seekbar_aosp);
+        } else if (customVolumeStyles == 1) {
+              seekbarDrawable =
+                      (LayerDrawable) mContext.getDrawable(R.drawable.volume_row_seekbar_rui);
+        } else {
+              seekbarDrawable =
+                      (LayerDrawable) mContext.getDrawable(R.drawable.volume_row_seekbar_rice);
+        }
 
         final LayerDrawable seekbarProgressDrawable = (LayerDrawable)
                 ((RoundedCornerProgressDrawable) seekbarDrawable.findDrawableByLayerId(
@@ -2461,12 +2491,13 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
                 ? Color.alpha(colorTint.getDefaultColor())
                 : getAlphaAttr(android.R.attr.disabledAlpha);
 
+        final ColorStateList colorTint2 = useActiveColoring
+                ? Utils.getColorAccent(mContext)
+                : Utils.getColorAttr(mContext, com.android.internal.R.attr.colorAccentCustom);
+
         final ColorStateList bgTint = useActiveColoring
                 ? Utils.getColorAttr(mContext, android.R.attr.colorBackgroundFloating)
                 : Utils.getColorAttr(mContext, com.android.internal.R.attr.colorAccentCustom);
-
-        final ColorStateList inverseTextTint = Utils.getColorAttr(
-                mContext, com.android.internal.R.attr.colorAccentCustom);
 
         row.sliderProgressSolid.setTintList(colorTint);
         if (row.sliderProgressIcon != null) {
@@ -2474,8 +2505,7 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
         }
 
         if (row.icon != null) {
-            row.icon.setImageTintList(inverseTextTint);
-            row.icon.setImageAlpha(alpha);
+            row.icon.setImageTintList(customVolumeStyles >= 1 ? colorTint : colorTint2);
         }
 
         if (row.number != null) {
