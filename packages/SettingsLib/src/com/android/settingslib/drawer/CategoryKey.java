c3d879eada2740aad679587130dab7b02e11b9cd
elpaablo <donopwhile1@gmail.com>
2023-03-30 18:30:20 +0100

    SettingsLib: add category for Alpha settings
    Change-Id: I6ce0290bdb8496c2d92cfb444f4c2e7a35db05d8

--- packages/SettingsLib/src/com/android/settingslib/drawer/CategoryKey.java
+++ packages/SettingsLib/src/com/android/settingslib/drawer/CategoryKey.java
@@ -74,8 +74,8 @@ public final class CategoryKey {
             "com.android.settings.category.ia.more_security_privacy_settings";
     public static final String CATEGORY_OTHER =
             "com.android.settings.category.ia.other";
-    public static final String CATEGORY_CRDROID =
-            "com.android.settings.category.ia.crdroid";
+    public static final String CATEGORY_ALPHA_SETTINGS =
+            "com.android.settings.category.ia.alpha";
 
     public static final Map<String, String> KEY_COMPAT_MAP;
 
