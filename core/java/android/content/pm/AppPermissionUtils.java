4d5fb64fe7f5649aba397bad2a5ea72f4b7b572c
U STARK <53818094+ul131web@users.noreply.github.com>
2023-04-27 20:46:05 +0530

    Create AppPermissionUtils.java
    Storage Scopes

---
+++ core/java/android/content/pm/AppPermissionUtils.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (C) 2022 GrapheneOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.content.pm;
+
+import android.Manifest;
+
+import com.android.internal.app.StorageScopesAppHooks;
+
+import static android.content.pm.GosPackageState.*;
+
+/** @hide */
+public class AppPermissionUtils {
+
+    // android.app.ApplicationPackageManager#checkPermission(String permName, String pkgName)
+    // android.app.ContextImpl#checkPermission(String permission, int pid, int uid)
+    public static boolean shouldSpoofSelfCheck(String permName) {
+        if (StorageScopesAppHooks.shouldSpoofSelfPermissionCheck(permName)) {
+            return true;
+        }
+
+        if (Manifest.permission.INTERNET.equals(permName)
+                && SpecialRuntimePermAppUtils.requestsInternetPermission()
+                && !SpecialRuntimePermAppUtils.awareOfRuntimeInternetPermission())
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    // android.app.AppOpsManager#checkOpNoThrow
+    // android.app.AppOpsManager#noteOpNoThrow
+    // android.app.AppOpsManager#noteProxyOpNoThrow
+    // android.app.AppOpsManager#unsafeCheckOpRawNoThrow
+    public static boolean shouldSpoofSelfAppOpCheck(int op) {
+        if (StorageScopesAppHooks.shouldSpoofSelfAppOpCheck(op)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static int getSpoofableStorageRuntimePermissionDflag(String permName) {
+        switch (permName) {
+            case Manifest.permission.READ_EXTERNAL_STORAGE:
+                return DFLAG_HAS_READ_EXTERNAL_STORAGE_DECLARATION;
+
+            case Manifest.permission.WRITE_EXTERNAL_STORAGE:
+                return DFLAG_HAS_WRITE_EXTERNAL_STORAGE_DECLARATION;
+
+            case Manifest.permission.ACCESS_MEDIA_LOCATION:
+                return DFLAG_HAS_ACCESS_MEDIA_LOCATION_DECLARATION;
+
+            case Manifest.permission.READ_MEDIA_AUDIO:
+                return DFLAG_HAS_READ_MEDIA_AUDIO_DECLARATION;
+
+            case Manifest.permission.READ_MEDIA_IMAGES:
+                return DFLAG_HAS_READ_MEDIA_IMAGES_DECLARATION;
+
+            case Manifest.permission.READ_MEDIA_VIDEO:
+                return DFLAG_HAS_READ_MEDIA_VIDEO_DECLARATION;
+
+            default:
+                return 0;
+        }
+    }
+}
