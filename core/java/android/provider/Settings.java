440b32eb15e8ebc8e20d3ef5102f09e5bf4709ff
Pranav Vashi <neobuddy89@gmail.com>
2023-06-30 17:03:36 +0530

    Add toggle to enable brightness slider haptic feedback [1/2]
    * Adapt to new QS compose
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7211,6 +7211,12 @@ public final class Settings {
         public static final String ON_THE_GO_CAMERA = "on_the_go_camera";
 
         /**
+         * Haptic feedback on brightness slider
+         * @hide
+         */
+        public static final String QS_BRIGHTNESS_SLIDER_HAPTIC = "qs_brightness_slider_haptic";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
