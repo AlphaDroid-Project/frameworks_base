2ad956c9d1a78866a306a64fb7a7892bb3f70f29
rdx420 <padraramesh420@gmail.com>
2023-09-06 15:36:22 +0530

    SystemUI: Add Resizable QS header Space[1/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6489,6 +6489,12 @@ public final class Settings {
          */
         public static final String QS_HEADER_CLOCK_SIZE = "qs_header_clock_size";
 
+        /**
+          * QS header size
+          * @hide
+          */
+        public static final String QS_HEADER_SIZE = "qs_header_size";
+
        /**
          * These are all public system settings
          *
@@ -6647,6 +6653,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(QS_HEADER_IMAGE_PADDING_TOP);
             PRIVATE_SETTINGS.add(QS_HEADER_IMAGE_URI);
             PRIVATE_SETTINGS.add(SHOW_BRIGHTNESS_BAR_PERCENTAGE);
+            PRIVATE_SETTINGS.add(QS_HEADER_SIZE);
         }
 
         /**
--- packages/SystemUI/src/com/android/systemui/qs/QSContainerImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSContainerImpl.java
@@ -26,6 +26,7 @@ import android.util.AttributeSet;
 import android.view.View;
 import android.widget.FrameLayout;
 
+import android.provider.Settings;
 import com.android.systemui.Dumpable;
 import com.android.systemui.R;
 import com.android.systemui.qs.customize.QSCustomizer;
@@ -146,8 +147,8 @@ public class QSContainerImpl extends FrameLayout implements Dumpable {
             QuickStatusBarHeaderController quickStatusBarHeaderController) {
         int topPadding = QSUtils.getQsHeaderSystemIconsAreaHeight(mContext);
         if (!LargeScreenUtils.shouldUseLargeScreenShadeHeader(mContext.getResources())) {
-            topPadding = mContext.getResources()
-                    .getDimensionPixelSize(R.dimen.large_screen_shade_header_height);
+            topPadding = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.QS_HEADER_SIZE, 110);
         }
         mQSPanelContainer.setPaddingRelative(
                 mQSPanelContainer.getPaddingStart(),
