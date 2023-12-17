5a351e2f5b34edc58437b1b8dd443a6be55e27d3
minaripenguin <minaripenguin@users.noreply.github.com>
2023-12-17 20:39:23 +0900

    [SQUASH] Integrate Pixel framework hooks and wrappers
    * included commits:
    
    Integrate Pixel framework hooks and wrappers
    
    [someone5678]
    * Add collapseShade, animateCollapseShade, and animateExpandNotificationsPanel
      for Quick Tap gesture
    
    Change-Id: I6f4203391d544205ea77107303fe2a5b1468e3c7
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: someone5678 <someone5678@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Alvin Francis <nivlafx@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    ref: https://github.com/crdroidandroid/android_frameworks_base/commit/dd83f0dc86388da37381f14ca71743c7356693c5
    
    Add required priv-app permissions for SystemUIGoogle
    
    Fixes:
    
    11-25 01:42:00.604  2784  2784 E System  : ************ Failure starting system services
    11-25 01:42:00.604  2784  2784 E System  : java.lang.IllegalStateException: Signature|privileged permissions not in privapp-permissions allowlist: {com.android.systemui (/system_ext/priv-app/SystemUIGoogle): android.permission.ACCESS_CONTEXT_HUB, com.android.systemui (/system_ext/priv-app/SystemUIGoogle): android.permission.REGISTER_STATS_PULL_ATOM, com.android.systemui (/system_ext/priv-app/SystemUIGoogle): android.permission.SET_WALLPAPER_COMPONENT}
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    ref: https://github.com/crdroidandroid/android_frameworks_base/commit/8339b43610fc51cd0f583cfa141fd7f98278bd9f
    
    Update pixel framework hooks and wrappers
    
    * Required by Ambient Music support
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    ref: https://github.com/crdroidandroid/android_frameworks_base/commit/4cb916696b5ff8be657dbef7b0821348c707f4d0

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -378,7 +378,7 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
     /** The current squish amount for the predictive back animation */
     private float mCurrentBackProgress = 0.0f;
     @Deprecated
-    private KeyguardBottomAreaView mKeyguardBottomArea;
+    public KeyguardBottomAreaView mKeyguardBottomArea;
     private boolean mExpanding;
     private boolean mSplitShadeEnabled;
     /** The bottom padding reserved for elements of the keyguard measuring notifications. */
@@ -1571,6 +1571,10 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
             mNotificationStackScrollLayoutController.setMaxDisplayedNotifications(-1);
             mNotificationStackScrollLayoutController.setKeyguardBottomPaddingForDebug(-1f);
         }
+    }
+
+    public void updateMaxDisplayedNotificationsWrapper(boolean recompute) {
+        updateMaxDisplayedNotifications(recompute);
     }
 
     private boolean shouldAvoidChangingNotificationsCount() {
@@ -3289,6 +3293,14 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
         mNotificationStackScrollLayoutController.setPulsing(pulsing, animatePulse);
 
         updateKeyguardStatusViewAlignment(/* animate= */ true);
+    }
+
+    public void setAmbientIndicationBottomPadding(int bottomPadding) {
+        mAmbientIndicationBottomPadding = bottomPadding;
+    }
+
+    public int getAmbientIndicationBottomPadding() {
+        return mAmbientIndicationBottomPadding;
     }
 
     @Override
@@ -5395,6 +5407,16 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
             }
             return super.performAccessibilityAction(host, action, args);
         }
+    }
+
+    @Override
+    public NotificationStackScrollLayoutController getScrollerLayoutController() {
+        return mNotificationStackScrollLayoutController;
+    }
+
+    @Override
+    public KeyguardBottomAreaView getKeyguardBottomAreaView() {
+        return mKeyguardBottomArea;
     }
 }
 
