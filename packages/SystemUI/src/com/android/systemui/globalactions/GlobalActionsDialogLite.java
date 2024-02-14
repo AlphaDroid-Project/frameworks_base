71a324c99654e559443036d6e6d713fd038a88f0
Timi Rautamäki <timi.rautamaki@gmail.com>
2024-02-14 18:00:30 +0000

    SystemUI: make powermenu usable on TV
    Change-Id: Id15ab94cfdc4fb8933ecc1b2c29b84a691df60d9

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialogLite.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialogLite.java
@@ -2943,6 +2943,8 @@ public class GlobalActionsDialogLite implements DialogInterface.OnDismissListene
                 GradientColors colors = mColorExtractor.getNeutralColors();
                 updateColors(colors, false /* animate */);
             }
+
+            mGlobalActionsLayout.getChildAt(0).requestFocus();
         }
 
         /**
