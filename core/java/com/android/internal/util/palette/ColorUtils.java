SuperiorExtended/android_frameworks_base

SystemUI: Implement OnePlus Style QS Panel
Special Thanks to @Corex1402

Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
Co-authored-by: Mr_ZerO <rbesar210311@gmail.com>
Co-authored-by: Subratam6296 <padraramesh420@gmail.com>
Change-Id: I68383f6f396c6a378db5fcad6d7d82014274e734

packages/SystemUI/src/com/superior/org/utils/palette/ColorUtils.java
@@ -1,0 +1,177 @@
+package com.superior.org.utils.palette;
+
+import android.graphics.Color;
+
+public class ColorUtils {
+    public static float constrain(float f, float f2, float f3) {
+        return f < f2 ? f2 : f > f3 ? f3 : f;
+    }
+
+    public static int constrain(int i, int i2, int i3) {
+        return i < i2 ? i2 : i > i3 ? i3 : i;
+    }
+
+    public static int compositeColors(int i, int i2) {
+        int alpha = Color.alpha(i2);
+        int alpha2 = Color.alpha(i);
+        int compositeAlpha = compositeAlpha(alpha2, alpha);
+        return Color.argb(compositeAlpha, compositeComponent(Color.red(i), alpha2, Color.red(i2), alpha, compositeAlpha), compositeComponent(Color.green(i), alpha2, Color.green(i2), alpha, compositeAlpha), compositeComponent(Color.blue(i), alpha2, Color.blue(i2), alpha, compositeAlpha));
+    }
+
+    public static int compositeAlpha(int i, int i2) {
+        return 255 - (((255 - i2) * (255 - i)) / 255);
+    }
+
+    public static int compositeComponent(int i, int i2, int i3, int i4, int i5) {
+        if (i5 == 0) {
+            return 0;
+        }
+        return (((i * 255) * i2) + ((i3 * i4) * (255 - i2))) / (i5 * 255);
+    }
+
+    public static double calculateLuminance(int i) {
+        double d;
+        double d2;
+        double d3;
+        double red = ((double) Color.red(i)) / 255.0d;
+        if (red < 0.03928d) {
+            d = red / 12.92d;
+        } else {
+            d = Math.pow((red + 0.055d) / 1.055d, 2.4d);
+        }
+        double green = ((double) Color.green(i)) / 255.0d;
+        if (green < 0.03928d) {
+            d2 = green / 12.92d;
+        } else {
+            d2 = Math.pow((green + 0.055d) / 1.055d, 2.4d);
+        }
+        double blue = ((double) Color.blue(i)) / 255.0d;
+        if (blue < 0.03928d) {
+            d3 = blue / 12.92d;
+        } else {
+            d3 = Math.pow((blue + 0.055d) / 1.055d, 2.4d);
+        }
+        return (d * 0.2126d) + (d2 * 0.7152d) + (d3 * 0.0722d);
+    }
+
+    public static double calculateContrast(int i, int i2) {
+        if (Color.alpha(i2) == 255) {
+            if (Color.alpha(i) < 255) {
+                i = compositeColors(i, i2);
+            }
+            double calculateLuminance = calculateLuminance(i) + 0.05d;
+            double calculateLuminance2 = calculateLuminance(i2) + 0.05d;
+            return Math.max(calculateLuminance, calculateLuminance2) / Math.min(calculateLuminance, calculateLuminance2);
+        }
+        throw new IllegalArgumentException("background can not be translucent");
+    }
+
+    public static int calculateMinimumAlpha(int i, int i2, float f) {
+        int i3 = 255;
+        if (Color.alpha(i2) == 255) {
+            double d = (double) f;
+            if (calculateContrast(setAlphaComponent(i, 255), i2) < d) {
+                return -1;
+            }
+            int i4 = 0;
+            for (int i5 = 0; i5 <= 10 && i3 - i4 > 10; i5++) {
+                int i6 = (i4 + i3) / 2;
+                if (calculateContrast(setAlphaComponent(i, i6), i2) < d) {
+                    i4 = i6;
+                } else {
+                    i3 = i6;
+                }
+            }
+            return i3;
+        }
+        throw new IllegalArgumentException("background can not be translucent");
+    }
+
+    public static void RGBToHSL(int i, int i2, int i3, float[] fArr) {
+        float f;
+        float f2;
+        float f3 = ((float) i) / 255.0f;
+        float f4 = ((float) i2) / 255.0f;
+        float f5 = ((float) i3) / 255.0f;
+        float max = Math.max(f3, Math.max(f4, f5));
+        float min = Math.min(f3, Math.min(f4, f5));
+        float f6 = max - min;
+        float f7 = (max + min) / 2.0f;
+        if (max == min) {
+            f = 0.0f;
+            f2 = 0.0f;
+        } else {
+            f = max == f3 ? ((f4 - f5) / f6) % 6.0f : max == f4 ? ((f5 - f3) / f6) + 2.0f : 4.0f + ((f3 - f4) / f6);
+            f2 = f6 / (1.0f - Math.abs((2.0f * f7) - 1.0f));
+        }
+        float f8 = (f * 60.0f) % 360.0f;
+        if (f8 < 0.0f) {
+            f8 += 360.0f;
+        }
+        fArr[0] = constrain(f8, 0.0f, 360.0f);
+        fArr[1] = constrain(f2, 0.0f, 1.0f);
+        fArr[2] = constrain(f7, 0.0f, 1.0f);
+    }
+
+    public static void colorToHSL(int i, float[] fArr) {
+        RGBToHSL(Color.red(i), Color.green(i), Color.blue(i), fArr);
+    }
+
+    public static int HSLToColor(float[] fArr) {
+        int i;
+        int i2;
+        int i3;
+        float f = fArr[0];
+        float f2 = fArr[1];
+        float f3 = fArr[2];
+        float abs = (1.0f - Math.abs((f3 * 2.0f) - 1.0f)) * f2;
+        float f4 = f3 - (0.5f * abs);
+        float abs2 = (1.0f - Math.abs(((f / 60.0f) % 2.0f) - 1.0f)) * abs;
+        switch (((int) f) / 60) {
+            case 0:
+                i3 = Math.round((abs + f4) * 255.0f);
+                i2 = Math.round((abs2 + f4) * 255.0f);
+                i = Math.round(f4 * 255.0f);
+                break;
+            case 1:
+                i3 = Math.round((abs2 + f4) * 255.0f);
+                i2 = Math.round((abs + f4) * 255.0f);
+                i = Math.round(f4 * 255.0f);
+                break;
+            case 2:
+                i3 = Math.round(f4 * 255.0f);
+                i2 = Math.round((abs + f4) * 255.0f);
+                i = Math.round((abs2 + f4) * 255.0f);
+                break;
+            case 3:
+                i3 = Math.round(f4 * 255.0f);
+                i2 = Math.round((abs2 + f4) * 255.0f);
+                i = Math.round((abs + f4) * 255.0f);
+                break;
+            case 4:
+                i3 = Math.round((abs2 + f4) * 255.0f);
+                i2 = Math.round(f4 * 255.0f);
+                i = Math.round((abs + f4) * 255.0f);
+                break;
+            case 5:
+            case 6:
+                i3 = Math.round((abs + f4) * 255.0f);
+                i2 = Math.round(f4 * 255.0f);
+                i = Math.round((abs2 + f4) * 255.0f);
+                break;
+            default:
+                i = 0;
+                i3 = 0;
+                i2 = 0;
+                break;
+        }
+        return Color.rgb(constrain(i3, 0, 255), constrain(i2, 0, 255), constrain(i, 0, 255));
+    }
+
+    public static int setAlphaComponent(int i, int i2) {
+        if (i2 >= 0 && i2 <= 255) {
+            return (i & 16777215) | (i2 << 24);
+        }
+        throw new IllegalArgumentException("alpha must be between 0 and 255.");
+    }
+}

packages/SystemUI/src/com/superior/org/utils/palette/DefaultGenerator.java
@@ -1,0 +1,113 @@
+package com.superior.org.utils.palette;
+
+import java.util.List;
+import com.superior.org.utils.palette.Palette;
+
+public class DefaultGenerator extends Palette.Generator {
+    public Palette.Swatch mDarkMutedSwatch;
+    public Palette.Swatch mDarkVibrantSwatch;
+    public int mHighestPopulation;
+    public Palette.Swatch mLightMutedSwatch;
+    public Palette.Swatch mLightVibrantSwatch;
+    public Palette.Swatch mMutedSwatch;
+    public List<Palette.Swatch> mSwatches;
+    public Palette.Swatch mVibrantSwatch;
+
+    public void generate(List<Palette.Swatch> list) {
+        this.mSwatches = list;
+        this.mHighestPopulation = findMaxPopulation();
+        generateVariationColors();
+        generateEmptySwatches();
+    }
+
+    public Palette.Swatch getLightVibrantSwatch() {
+        return this.mLightVibrantSwatch;
+    }
+
+    public Palette.Swatch getDarkVibrantSwatch() {
+        return this.mDarkVibrantSwatch;
+    }
+
+    public final void generateVariationColors() {
+        this.mVibrantSwatch = findColorVariation(0.5f, 0.3f, 0.7f, 1.0f, 0.35f, 1.0f);
+        this.mLightVibrantSwatch = findColorVariation(0.74f, 0.55f, 1.0f, 1.0f, 0.35f, 1.0f);
+        this.mDarkVibrantSwatch = findColorVariation(0.26f, 0.0f, 0.45f, 1.0f, 0.35f, 1.0f);
+        this.mMutedSwatch = findColorVariation(0.5f, 0.3f, 0.7f, 0.3f, 0.0f, 0.4f);
+        this.mLightMutedSwatch = findColorVariation(0.74f, 0.55f, 1.0f, 0.3f, 0.0f, 0.4f);
+        this.mDarkMutedSwatch = findColorVariation(0.26f, 0.0f, 0.45f, 0.3f, 0.0f, 0.4f);
+    }
+
+    public final void generateEmptySwatches() {
+        Palette.Swatch swatch;
+        Palette.Swatch swatch2;
+        if (this.mVibrantSwatch == null && (swatch2 = this.mDarkVibrantSwatch) != null) {
+            float[] copyHslValues = copyHslValues(swatch2);
+            copyHslValues[2] = 0.5f;
+            this.mVibrantSwatch = new Palette.Swatch(ColorUtils.HSLToColor(copyHslValues), 0);
+        }
+        if (this.mDarkVibrantSwatch == null && (swatch = this.mVibrantSwatch) != null) {
+            float[] copyHslValues2 = copyHslValues(swatch);
+            copyHslValues2[2] = 0.26f;
+            this.mDarkVibrantSwatch = new Palette.Swatch(ColorUtils.HSLToColor(copyHslValues2), 0);
+        }
+    }
+
+    public final int findMaxPopulation() {
+        int i = 0;
+        for (Palette.Swatch population : this.mSwatches) {
+            i = Math.max(i, population.getPopulation());
+        }
+        return i;
+    }
+
+    public final Palette.Swatch findColorVariation(float f, float f2, float f3, float f4, float f5, float f6) {
+        Palette.Swatch swatch = null;
+        float f7 = 0.0f;
+        for (Palette.Swatch next : this.mSwatches) {
+            float f8 = next.getHsl()[1];
+            float f9 = next.getHsl()[2];
+            if (f8 >= f5 && f8 <= f6 && f9 >= f2 && f9 <= f3 && !isAlreadySelected(next)) {
+                float createComparisonValue = createComparisonValue(f8, f4, f9, f, next.getPopulation(), this.mHighestPopulation);
+                if (swatch == null || createComparisonValue > f7) {
+                    swatch = next;
+                    f7 = createComparisonValue;
+                }
+            }
+        }
+        return swatch;
+    }
+
+    public final boolean isAlreadySelected(Palette.Swatch swatch) {
+        return this.mVibrantSwatch == swatch || this.mDarkVibrantSwatch == swatch || this.mLightVibrantSwatch == swatch || this.mMutedSwatch == swatch || this.mDarkMutedSwatch == swatch || this.mLightMutedSwatch == swatch;
+    }
+
+    public static float createComparisonValue(float f, float f2, float f3, float f4, int i, int i2) {
+        return createComparisonValue(f, f2, 3.0f, f3, f4, 6.0f, i, i2, 1.0f);
+    }
+
+    public static float createComparisonValue(float f, float f2, float f3, float f4, float f5, float f6, int i, int i2, float f7) {
+        return weightedMean(invertDiff(f, f2), f3, invertDiff(f4, f5), f6, ((float) i) / ((float) i2), f7);
+    }
+
+    public static float[] copyHslValues(Palette.Swatch swatch) {
+        float[] fArr = new float[3];
+        System.arraycopy(swatch.getHsl(), 0, fArr, 0, 3);
+        return fArr;
+    }
+
+    public static float invertDiff(float f, float f2) {
+        return 1.0f - Math.abs(f - f2);
+    }
+
+    public static float weightedMean(float... fArr) {
+        float f = 0.0f;
+        float f2 = 0.0f;
+        for (int i = 0; i < fArr.length; i += 2) {
+            float f3 = fArr[i];
+            float f4 = fArr[i + 1];
+            f += f3 * f4;
+            f2 += f4;
+        }
+        return f / f2;
+    }
+}

AlphaDroid-Project/frameworks_base

core/java/com/android/internal/util/palette/ColorUtils.java
@@ -1,0 +1,177 @@
+package com.android.internal.util.palette;
+
+import android.graphics.Color;
+
+public class ColorUtils {
+    public static float constrain(float f, float f2, float f3) {
+        return f < f2 ? f2 : f > f3 ? f3 : f;
+    }
+
+    public static int constrain(int i, int i2, int i3) {
+        return i < i2 ? i2 : i > i3 ? i3 : i;
+    }
+
+    public static int compositeColors(int i, int i2) {
+        int alpha = Color.alpha(i2);
+        int alpha2 = Color.alpha(i);
+        int compositeAlpha = compositeAlpha(alpha2, alpha);
+        return Color.argb(compositeAlpha, compositeComponent(Color.red(i), alpha2, Color.red(i2), alpha, compositeAlpha), compositeComponent(Color.green(i), alpha2, Color.green(i2), alpha, compositeAlpha), compositeComponent(Color.blue(i), alpha2, Color.blue(i2), alpha, compositeAlpha));
+    }
+
+    public static int compositeAlpha(int i, int i2) {
+        return 255 - (((255 - i2) * (255 - i)) / 255);
+    }
+
+    public static int compositeComponent(int i, int i2, int i3, int i4, int i5) {
+        if (i5 == 0) {
+            return 0;
+        }
+        return (((i * 255) * i2) + ((i3 * i4) * (255 - i2))) / (i5 * 255);
+    }
+
+    public static double calculateLuminance(int i) {
+        double d;
+        double d2;
+        double d3;
+        double red = ((double) Color.red(i)) / 255.0d;
+        if (red < 0.03928d) {
+            d = red / 12.92d;
+        } else {
+            d = Math.pow((red + 0.055d) / 1.055d, 2.4d);
+        }
+        double green = ((double) Color.green(i)) / 255.0d;
+        if (green < 0.03928d) {
+            d2 = green / 12.92d;
+        } else {
+            d2 = Math.pow((green + 0.055d) / 1.055d, 2.4d);
+        }
+        double blue = ((double) Color.blue(i)) / 255.0d;
+        if (blue < 0.03928d) {
+            d3 = blue / 12.92d;
+        } else {
+            d3 = Math.pow((blue + 0.055d) / 1.055d, 2.4d);
+        }
+        return (d * 0.2126d) + (d2 * 0.7152d) + (d3 * 0.0722d);
+    }
+
+    public static double calculateContrast(int i, int i2) {
+        if (Color.alpha(i2) == 255) {
+            if (Color.alpha(i) < 255) {
+                i = compositeColors(i, i2);
+            }
+            double calculateLuminance = calculateLuminance(i) + 0.05d;
+            double calculateLuminance2 = calculateLuminance(i2) + 0.05d;
+            return Math.max(calculateLuminance, calculateLuminance2) / Math.min(calculateLuminance, calculateLuminance2);
+        }
+        throw new IllegalArgumentException("background can not be translucent");
+    }
+
+    public static int calculateMinimumAlpha(int i, int i2, float f) {
+        int i3 = 255;
+        if (Color.alpha(i2) == 255) {
+            double d = (double) f;
+            if (calculateContrast(setAlphaComponent(i, 255), i2) < d) {
+                return -1;
+            }
+            int i4 = 0;
+            for (int i5 = 0; i5 <= 10 && i3 - i4 > 10; i5++) {
+                int i6 = (i4 + i3) / 2;
+                if (calculateContrast(setAlphaComponent(i, i6), i2) < d) {
+                    i4 = i6;
+                } else {
+                    i3 = i6;
+                }
+            }
+            return i3;
+        }
+        throw new IllegalArgumentException("background can not be translucent");
+    }
+
+    public static void RGBToHSL(int i, int i2, int i3, float[] fArr) {
+        float f;
+        float f2;
+        float f3 = ((float) i) / 255.0f;
+        float f4 = ((float) i2) / 255.0f;
+        float f5 = ((float) i3) / 255.0f;
+        float max = Math.max(f3, Math.max(f4, f5));
+        float min = Math.min(f3, Math.min(f4, f5));
+        float f6 = max - min;
+        float f7 = (max + min) / 2.0f;
+        if (max == min) {
+            f = 0.0f;
+            f2 = 0.0f;
+        } else {
+            f = max == f3 ? ((f4 - f5) / f6) % 6.0f : max == f4 ? ((f5 - f3) / f6) + 2.0f : 4.0f + ((f3 - f4) / f6);
+            f2 = f6 / (1.0f - Math.abs((2.0f * f7) - 1.0f));
+        }
+        float f8 = (f * 60.0f) % 360.0f;
+        if (f8 < 0.0f) {
+            f8 += 360.0f;
+        }
+        fArr[0] = constrain(f8, 0.0f, 360.0f);
+        fArr[1] = constrain(f2, 0.0f, 1.0f);
+        fArr[2] = constrain(f7, 0.0f, 1.0f);
+    }
+
+    public static void colorToHSL(int i, float[] fArr) {
+        RGBToHSL(Color.red(i), Color.green(i), Color.blue(i), fArr);
+    }
+
+    public static int HSLToColor(float[] fArr) {
+        int i;
+        int i2;
+        int i3;
+        float f = fArr[0];
+        float f2 = fArr[1];
+        float f3 = fArr[2];
+        float abs = (1.0f - Math.abs((f3 * 2.0f) - 1.0f)) * f2;
+        float f4 = f3 - (0.5f * abs);
+        float abs2 = (1.0f - Math.abs(((f / 60.0f) % 2.0f) - 1.0f)) * abs;
+        switch (((int) f) / 60) {
+            case 0:
+                i3 = Math.round((abs + f4) * 255.0f);
+                i2 = Math.round((abs2 + f4) * 255.0f);
+                i = Math.round(f4 * 255.0f);
+                break;
+            case 1:
+                i3 = Math.round((abs2 + f4) * 255.0f);
+                i2 = Math.round((abs + f4) * 255.0f);
+                i = Math.round(f4 * 255.0f);
+                break;
+            case 2:
+                i3 = Math.round(f4 * 255.0f);
+                i2 = Math.round((abs + f4) * 255.0f);
+                i = Math.round((abs2 + f4) * 255.0f);
+                break;
+            case 3:
+                i3 = Math.round(f4 * 255.0f);
+                i2 = Math.round((abs2 + f4) * 255.0f);
+                i = Math.round((abs + f4) * 255.0f);
+                break;
+            case 4:
+                i3 = Math.round((abs2 + f4) * 255.0f);
+                i2 = Math.round(f4 * 255.0f);
+                i = Math.round((abs + f4) * 255.0f);
+                break;
+            case 5:
+            case 6:
+                i3 = Math.round((abs + f4) * 255.0f);
+                i2 = Math.round(f4 * 255.0f);
+                i = Math.round((abs2 + f4) * 255.0f);
+                break;
+            default:
+                i = 0;
+                i3 = 0;
+                i2 = 0;
+                break;
+        }
+        return Color.rgb(constrain(i3, 0, 255), constrain(i2, 0, 255), constrain(i, 0, 255));
+    }
+
+    public static int setAlphaComponent(int i, int i2) {
+        if (i2 >= 0 && i2 <= 255) {
+            return (i & 16777215) | (i2 << 24);
+        }
+        throw new IllegalArgumentException("alpha must be between 0 and 255.");
+    }
+}

core/java/com/android/internal/util/palette/DefaultGenerator.java
@@ -1,0 +1,113 @@
+package com.android.internal.util.palette;
+
+import java.util.List;
+import com.android.internal.util.palette.Palette;
+
+public class DefaultGenerator extends Palette.Generator {
+    public Palette.Swatch mDarkMutedSwatch;
+    public Palette.Swatch mDarkVibrantSwatch;
+    public int mHighestPopulation;
+    public Palette.Swatch mLightMutedSwatch;
+    public Palette.Swatch mLightVibrantSwatch;
+    public Palette.Swatch mMutedSwatch;
+    public List<Palette.Swatch> mSwatches;
+    public Palette.Swatch mVibrantSwatch;
+
+    public void generate(List<Palette.Swatch> list) {
+        this.mSwatches = list;
+        this.mHighestPopulation = findMaxPopulation();
+        generateVariationColors();
+        generateEmptySwatches();
+    }
+
+    public Palette.Swatch getLightVibrantSwatch() {
+        return this.mLightVibrantSwatch;
+    }
+
+    public Palette.Swatch getDarkVibrantSwatch() {
+        return this.mDarkVibrantSwatch;
+    }
+
+    public final void generateVariationColors() {
+        this.mVibrantSwatch = findColorVariation(0.5f, 0.3f, 0.7f, 1.0f, 0.35f, 1.0f);
+        this.mLightVibrantSwatch = findColorVariation(0.74f, 0.55f, 1.0f, 1.0f, 0.35f, 1.0f);
+        this.mDarkVibrantSwatch = findColorVariation(0.26f, 0.0f, 0.45f, 1.0f, 0.35f, 1.0f);
+        this.mMutedSwatch = findColorVariation(0.5f, 0.3f, 0.7f, 0.3f, 0.0f, 0.4f);
+        this.mLightMutedSwatch = findColorVariation(0.74f, 0.55f, 1.0f, 0.3f, 0.0f, 0.4f);
+        this.mDarkMutedSwatch = findColorVariation(0.26f, 0.0f, 0.45f, 0.3f, 0.0f, 0.4f);
+    }
+
+    public final void generateEmptySwatches() {
+        Palette.Swatch swatch;
+        Palette.Swatch swatch2;
+        if (this.mVibrantSwatch == null && (swatch2 = this.mDarkVibrantSwatch) != null) {
+            float[] copyHslValues = copyHslValues(swatch2);
+            copyHslValues[2] = 0.5f;
+            this.mVibrantSwatch = new Palette.Swatch(ColorUtils.HSLToColor(copyHslValues), 0);
+        }
+        if (this.mDarkVibrantSwatch == null && (swatch = this.mVibrantSwatch) != null) {
+            float[] copyHslValues2 = copyHslValues(swatch);
+            copyHslValues2[2] = 0.26f;
+            this.mDarkVibrantSwatch = new Palette.Swatch(ColorUtils.HSLToColor(copyHslValues2), 0);
+        }
+    }
+
+    public final int findMaxPopulation() {
+        int i = 0;
+        for (Palette.Swatch population : this.mSwatches) {
+            i = Math.max(i, population.getPopulation());
+        }
+        return i;
+    }
+
+    public final Palette.Swatch findColorVariation(float f, float f2, float f3, float f4, float f5, float f6) {
+        Palette.Swatch swatch = null;
+        float f7 = 0.0f;
+        for (Palette.Swatch next : this.mSwatches) {
+            float f8 = next.getHsl()[1];
+            float f9 = next.getHsl()[2];
+            if (f8 >= f5 && f8 <= f6 && f9 >= f2 && f9 <= f3 && !isAlreadySelected(next)) {
+                float createComparisonValue = createComparisonValue(f8, f4, f9, f, next.getPopulation(), this.mHighestPopulation);
+                if (swatch == null || createComparisonValue > f7) {
+                    swatch = next;
+                    f7 = createComparisonValue;
+                }
+            }
+        }
+        return swatch;
+    }
+
+    public final boolean isAlreadySelected(Palette.Swatch swatch) {
+        return this.mVibrantSwatch == swatch || this.mDarkVibrantSwatch == swatch || this.mLightVibrantSwatch == swatch || this.mMutedSwatch == swatch || this.mDarkMutedSwatch == swatch || this.mLightMutedSwatch == swatch;
+    }
+
+    public static float createComparisonValue(float f, float f2, float f3, float f4, int i, int i2) {
+        return createComparisonValue(f, f2, 3.0f, f3, f4, 6.0f, i, i2, 1.0f);
+    }
+
+    public static float createComparisonValue(float f, float f2, float f3, float f4, float f5, float f6, int i, int i2, float f7) {
+        return weightedMean(invertDiff(f, f2), f3, invertDiff(f4, f5), f6, ((float) i) / ((float) i2), f7);
+    }
+
+    public static float[] copyHslValues(Palette.Swatch swatch) {
+        float[] fArr = new float[3];
+        System.arraycopy(swatch.getHsl(), 0, fArr, 0, 3);
+        return fArr;
+    }
+
+    public static float invertDiff(float f, float f2) {
+        return 1.0f - Math.abs(f - f2);
+    }
+
+    public static float weightedMean(float... fArr) {
+        float f = 0.0f;
+        float f2 = 0.0f;
+        for (int i = 0; i < fArr.length; i += 2) {
+            float f3 = fArr[i];
+            float f4 = fArr[i + 1];
+            f += f3 * f4;
+            f2 += f4;
+        }
+        return f / f2;
+    }
+}