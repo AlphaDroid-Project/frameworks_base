6717a51abd9e79c8d774af7913787ee8cfe834cf
minaripenguin <minaripenguin@users.noreply.github.com>
2024-05-08 09:34:05 +0800

    VolumeDialogImpl: Add toggle for media output button on volume panel
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -367,6 +367,8 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
     private final VibratorHelper mVibratorHelper;
     private final com.android.systemui.util.time.SystemClock mSystemClock;
 
+    private boolean mShowMediaButton = true;
+
     public VolumeDialogImpl(
             Context context,
             VolumeDialogController volumeDialogController,
@@ -463,6 +465,17 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
                 Settings.Secure.getUriFor(Settings.Secure.VOLUME_DIALOG_DISMISS_TIMEOUT),
                 false, volumeTimeoutObserver);
         volumeTimeoutObserver.onChange(true);
+        ContentObserver volumeMediaButtonObserver = new ContentObserver(null) {
+            @Override
+            public void onChange(boolean selfChange) {
+                mShowMediaButton = mSecureSettings.get().getInt(
+                        "volume_show_media_button", 1) != 0;
+            }
+        };
+        mContext.getContentResolver().registerContentObserver(
+                Settings.Secure.getUriFor("volume_show_media_button"),
+                false, volumeMediaButtonObserver);
+        volumeMediaButtonObserver.onChange(true);
 
         initDimens();
 
@@ -1501,7 +1514,7 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
 
     private boolean isMediaControllerAvailable() {
         final MediaController mediaController = getActiveLocalMediaController();
-        return mediaController != null && !TextUtils.isEmpty(mediaController.getPackageName());
+        return mediaController != null && !TextUtils.isEmpty(mediaController.getPackageName()) && mShowMediaButton;
     }
 
     private void initSettingsH(int lockTaskModeState) {
