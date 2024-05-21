ec16b5b502654d75df609d2e96870d59137f2b89
someone5678 <someone5678@users.noreply.github.com>
2024-05-21 09:55:51 -0400

    base: Allow to hide screen capture status from apps [1/2]
    * Allow to hide screen capture detection from privacy invasive apps
    * This feature will be extended to screenrecord
      in the future release of Android (A15)
    
    Change-Id: Ie2a8e49614dcd8fce2311a644d615ab60670c094
    Signed-off-by: someone5678 <someone5678@users.noreply.github.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -22502,6 +22502,12 @@ public final class Settings {
         public static final String WINDOW_IGNORE_SECURE = "window_ignore_secure";
 
         /**
+         * Control whether to hide screen capture status from apps.
+         * @hide
+         */
+        public static final String HIDE_SCREEN_CAPTURE_STATUS = "hide_screen_capture_status";
+
+        /**
          * Setting indicating whether Low Power Standby is enabled, if supported.
          *
          * Values are:
