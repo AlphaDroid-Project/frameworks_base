26aac48297f3cccefd21071c3461ad1bceaa275c
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-10-16 22:01:36 +0300

    fix NPE system_server crash in IMMS.resetDefaultImeLocked()

--- services/core/java/com/android/server/inputmethod/InputMethodManagerService.java
+++ services/core/java/com/android/server/inputmethod/InputMethodManagerService.java
@@ -1363,9 +1363,11 @@ public final class InputMethodManagerService implements IInputMethodManagerImpl.
         // Do not reset the default (current) IME when it is a 3rd-party IME
         String selectedMethodId = bindingController.getSelectedMethodId();
         final InputMethodSettings settings = InputMethodSettingsRepository.get(userId);
-        if (selectedMethodId != null
-                && !settings.getMethodMap().get(selectedMethodId).isSystem()) {
-            return;
+        if (selectedMethodId != null) {
+            InputMethodInfo selectedMethod = settings.getMethodMap().get(selectedMethodId);
+            if (selectedMethod != null && !selectedMethod.isSystem()) {
+                return;
+            }
         }
         final List<InputMethodInfo> suitableImes = InputMethodInfoUtils.getDefaultEnabledImes(
                 context, settings.getEnabledInputMethodList());
