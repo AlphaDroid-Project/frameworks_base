RisingTechOSS/android_frameworks_base

BrightnessController: Prevent possible memory leak
Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

packages/SystemUI/src/com/android/systemui/settings/brightness/BrightnessController.java
@@ -120,6 +120,7 @@ public class BrightnessController implements ToggleSlider.Listener, MirroredBrig
     private static final VibrationEffect BRIGHTNESS_SLIDER_HAPTIC =
             VibrationEffect.get(VibrationEffect.EFFECT_TICK);
     private int mBrightnessSliderHaptic;
+    private final TunerService mTunerService;
 
     @Override
     public void setMirror(BrightnessMirrorController controller) {
@@ -308,6 +309,7 @@ public class BrightnessController implements ToggleSlider.Listener, MirroredBrig
         mDisplayId = mContext.getDisplayId();
         mDisplayManager = displayManager;
         mVrManager = iVrManager;
+        mTunerService = tunerService;
 
         mMainHandler = new Handler(mainLooper, mHandlerCallback);
 
@@ -322,8 +324,6 @@ public class BrightnessController implements ToggleSlider.Listener, MirroredBrig
                     Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL :
                     Settings.System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC,
                 UserHandle.USER_CURRENT));
-        tunerService.addTunable(this, SCREEN_BRIGHTNESS_MODE);
-        tunerService.addTunable(this, QS_BRIGHTNESS_SLIDER_HAPTIC);
     }
 
     public void registerCallbacks() {
@@ -336,6 +336,17 @@ public class BrightnessController implements ToggleSlider.Listener, MirroredBrig
         mBackgroundHandler.removeCallbacks(mStopListeningRunnable);
         mBackgroundHandler.post(mStopListeningRunnable);
         mControlValueInitialized = false;
+    }
+
+    public void addListeners() {
+        mTunerService.addTunable(this, SCREEN_BRIGHTNESS_MODE);
+        mTunerService.addTunable(this, QS_BRIGHTNESS_SLIDER_HAPTIC);
+    }
+    
+    public void removeListeners() {
+        mBackgroundHandler.removeCallbacksAndMessages(null);
+        mMainHandler.removeCallbacksAndMessages(null);
+        mTunerService.removeTunable(this);
     }
 
     @Override

AlphaDroid-Project/frameworks_base

packages/SystemUI/src/com/android/systemui/settings/brightness/BrightnessController.java
@@ -120,6 +120,7 @@ public class BrightnessController implements ToggleSlider.Listener, MirroredBrig
     private static final VibrationEffect BRIGHTNESS_SLIDER_HAPTIC =
             VibrationEffect.get(VibrationEffect.EFFECT_TICK);
     private int mBrightnessSliderHaptic;
+    private final TunerService mTunerService;
 
 
     @Override
@@ -309,6 +310,7 @@ public class BrightnessController implements ToggleSlider.Listener, MirroredBrig
         mDisplayId = mContext.getDisplayId();
         mDisplayManager = displayManager;
         mVrManager = iVrManager;
+        mTunerService = tunerService;
 
         mMainHandler = new Handler(mainLooper, mHandlerCallback);
 
@@ -323,8 +325,6 @@ public class BrightnessController implements ToggleSlider.Listener, MirroredBrig
                     Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL :
                     Settings.System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC,
                 UserHandle.USER_CURRENT));
-        tunerService.addTunable(this, SCREEN_BRIGHTNESS_MODE);
-        tunerService.addTunable(this, BRIGHTNESS_SLIDER_HAPTICS_INTENSITY);
     }
 
     public void registerCallbacks() {
@@ -337,6 +337,17 @@ public class BrightnessController implements ToggleSlider.Listener, MirroredBrig
         mBackgroundHandler.removeCallbacks(mStopListeningRunnable);
         mBackgroundHandler.post(mStopListeningRunnable);
         mControlValueInitialized = false;
+    }
+
+    public void addListeners() {
+        mTunerService.addTunable(this, SCREEN_BRIGHTNESS_MODE);
+        mTunerService.addTunable(this, BRIGHTNESS_SLIDER_HAPTICS_INTENSITY);
+    }
+
+    public void removeListeners() {
+        mBackgroundHandler.removeCallbacksAndMessages(null);
+        mMainHandler.removeCallbacksAndMessages(null);
+        mTunerService.removeTunable(this);
     }
 
     @Override