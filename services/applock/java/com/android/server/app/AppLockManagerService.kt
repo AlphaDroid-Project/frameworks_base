14211d4dc6d0f2807ab907fdb7a3ccd63f944e5e
minaripenguin <minaripenguin@users.noreply.github.com>
2024-10-30 14:53:03 +0800

    services: Introduce QuickSwitch feature [1/2]
    * initial feature rework
    * add support for pixel launcher
    
    [elpaablo]: give 'shouldBypassTaskPermission' a more generic name, since it's being used in several places.
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: elpaablo <donopwhile1@gmail.com>

--- services/applock/java/com/android/server/app/AppLockManagerService.kt
+++ services/applock/java/com/android/server/app/AppLockManagerService.kt
@@ -814,6 +814,9 @@ class AppLockManagerService(
     }
 
     private fun enforceCallingPermission(msg: String) {
+        if (com.android.internal.util.alpha.BypassUtils.isSystemLauncher(Binder.getCallingUid())) {
+            return
+        }
         context.enforceCallingPermission(Manifest.permission.MANAGE_APP_LOCK, msg)
     }
 
