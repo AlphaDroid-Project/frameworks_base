6e6127704114059a47f8bb8cffe939941a1aeddb
Michael Bestas <mkbestas@lineageos.org>
2024-12-03 00:17:00 +0200

    Merge tag 'android-security-14.0.0_r15' of https://android.googlesource.com/platform/frameworks/base into lineage-21.0
    Android Security 14.0.0 Release 15 (12496787)
    
    * tag 'android-security-14.0.0_r15' of https://android.googlesource.com/platform/frameworks/base:
      Allow uninstalling DMRH when not used for management
      Properly handle onNullBinding() in appwidget service.
      Block clipboard UI when device is locked
    
     Conflicts:
    	packages/SystemUI/src/com/android/systemui/clipboardoverlay/ClipboardListener.java
    	packages/SystemUI/tests/src/com/android/systemui/clipboardoverlay/ClipboardListenerTest.java
    
    Change-Id: I2bb529d8999199a655f970bff4aa80deac94d6ec

--- services/appwidget/java/com/android/server/appwidget/AppWidgetServiceImpl.java
+++ services/appwidget/java/com/android/server/appwidget/AppWidgetServiceImpl.java
@@ -1960,6 +1960,11 @@ class AppWidgetServiceImpl extends IAppWidgetService.Stub implements WidgetBacku
             }
 
             @Override
+            public void onNullBinding(ComponentName name) {
+                mContext.unbindService(this);
+            }
+
+            @Override
             public void onServiceDisconnected(ComponentName name) {
                 // Do nothing
             }
@@ -2101,6 +2106,11 @@ class AppWidgetServiceImpl extends IAppWidgetService.Stub implements WidgetBacku
                                 } catch (RemoteException e) {
                                     Slog.e(TAG, "Error calling onDataSetChangedAsync()", e);
                                 }
+                                mContext.unbindService(this);
+                            }
+
+                            @Override
+                            public void onNullBinding(ComponentName name) {
                                 mContext.unbindService(this);
                             }
 
