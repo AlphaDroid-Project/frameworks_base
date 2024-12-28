1c8ffbe10d3f36ab3e4fb5e5eba1b3012f1dc797
LuK1337 <priv.luk@gmail.com>
2024-12-28 15:41:01 +0100

    SystemUI: Fix constraints for keyguard slice view
    With this, the clock and date are aligned with each other properly on
    tablets.
    
    Change-Id: Ib0954182892501cddcb6988a6afc17f8c4208d6d

--- packages/SystemUI/src/com/android/systemui/keyguard/ui/view/layout/sections/KeyguardSliceViewSection.kt
+++ packages/SystemUI/src/com/android/systemui/keyguard/ui/view/layout/sections/KeyguardSliceViewSection.kt
@@ -17,6 +17,7 @@
 
 package com.android.systemui.keyguard.ui.view.layout.sections
 
+import android.content.Context
 import android.view.View
 import android.view.ViewGroup
 import androidx.constraintlayout.widget.Barrier
@@ -32,6 +33,7 @@ import javax.inject.Inject
 class KeyguardSliceViewSection
 @Inject
 constructor(
+    private val context: Context,
     val smartspaceController: LockscreenSmartspaceController,
 ) : KeyguardSection() {
     override fun addViews(constraintLayout: ConstraintLayout) {
@@ -55,7 +57,9 @@ constructor(
                 R.id.keyguard_slice_view,
                 ConstraintSet.START,
                 ConstraintSet.PARENT_ID,
-                ConstraintSet.START
+                ConstraintSet.START,
+                context.resources.getDimensionPixelSize(customR.dimen.clock_padding_start) +
+                    context.resources.getDimensionPixelSize(customR.dimen.status_view_margin_horizontal),
             )
             connect(
                 R.id.keyguard_slice_view,
