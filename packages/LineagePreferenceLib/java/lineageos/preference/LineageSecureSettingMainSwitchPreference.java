giste/beerdroid_lineage-sdk

Fix settings pref switch for multi-user
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

packages/LineagePreferenceLib/java/lineageos/preference/LineageSecureSettingMainSwitchPreference.java
@@ -17,6 +17,7 @@
 package lineageos.preference;
 
 import android.content.Context;
+import android.os.UserHandle;
 import android.util.AttributeSet;
 
 import androidx.preference.PreferenceDataStore;
@@ -46,13 +47,13 @@ public class LineageSecureSettingMainSwitchPreference extends MainSwitchPreferen
     private class DataStore extends PreferenceDataStore {
         @Override
         public void putBoolean(String key, boolean value) {
-            LineageSettings.Secure.putInt(getContext().getContentResolver(), key, value ? 1 : 0);
+            LineageSettings.Secure.putIntForUser(getContext().getContentResolver(), key, value ? 1 : 0, UserHandle.USER_CURRENT);
         }
 
         @Override
         public boolean getBoolean(String key, boolean defaultValue) {
-            return LineageSettings.Secure.getInt(getContext().getContentResolver(), key,
-                    defaultValue ? 1 : 0) != 0;
+            return LineageSettings.Secure.getIntForUser(getContext().getContentResolver(), key,
+                    defaultValue ? 1 : 0, UserHandle.USER_CURRENT) != 0;
         }
     }
 }

packages/LineagePreferenceLib/java/lineageos/preference/LineageSecureSettingSwitchPreference.java
@@ -18,6 +18,7 @@
 package lineageos.preference;
 
 import android.content.Context;
+import android.os.UserHandle;
 import android.util.AttributeSet;
 
 import lineageos.providers.LineageSettings;
@@ -43,12 +44,12 @@ public class LineageSecureSettingSwitchPreference extends SelfRemovingSwitchPref
 
     @Override
     protected void putBoolean(String key, boolean value) {
-        LineageSettings.Secure.putInt(getContext().getContentResolver(), key, value ? 1 : 0);
+        LineageSettings.Secure.putIntForUser(getContext().getContentResolver(), key, value ? 1 : 0, UserHandle.USER_CURRENT);
     }
 
     @Override
     protected boolean getBoolean(String key, boolean defaultValue) {
-        return LineageSettings.Secure.getInt(getContext().getContentResolver(),
-                key, defaultValue ? 1 : 0) != 0;
+        return LineageSettings.Secure.getIntForUser(getContext().getContentResolver(),
+                key, defaultValue ? 1 : 0, UserHandle.USER_CURRENT) != 0;
     }
 }

packages/LineagePreferenceLib/java/lineageos/preference/LineageSystemSettingMainSwitchPreference.java
@@ -17,6 +17,7 @@
 package lineageos.preference;
 
 import android.content.Context;
+import android.os.UserHandle;
 import android.util.AttributeSet;
 
 import androidx.preference.PreferenceDataStore;
@@ -46,13 +47,13 @@ public class LineageSystemSettingMainSwitchPreference extends MainSwitchPreferen
     private class DataStore extends PreferenceDataStore {
         @Override
         public void putBoolean(String key, boolean value) {
-            LineageSettings.System.putInt(getContext().getContentResolver(), key, value ? 1 : 0);
+            LineageSettings.System.putIntForUser(getContext().getContentResolver(), key, value ? 1 : 0, UserHandle.USER_CURRENT);
         }
 
         @Override
         public boolean getBoolean(String key, boolean defaultValue) {
-            return LineageSettings.System.getInt(getContext().getContentResolver(), key,
-                    defaultValue ? 1 : 0) != 0;
+            return LineageSettings.System.getIntForUser(getContext().getContentResolver(), key,
+                    defaultValue ? 1 : 0, UserHandle.USER_CURRENT) != 0;
         }
     }
 }

packages/LineagePreferenceLib/java/lineageos/preference/LineageSystemSettingSwitchPreference.java
@@ -18,6 +18,7 @@
 package lineageos.preference;
 
 import android.content.Context;
+import android.os.UserHandle;
 import android.util.AttributeSet;
 
 import lineageos.providers.LineageSettings;
@@ -43,12 +44,12 @@ public class LineageSystemSettingSwitchPreference extends SelfRemovingSwitchPref
 
     @Override
     protected void putBoolean(String key, boolean value) {
-        LineageSettings.System.putInt(getContext().getContentResolver(), key, value ? 1 : 0);
+        LineageSettings.System.putIntForUser(getContext().getContentResolver(), key, value ? 1 : 0, UserHandle.USER_CURRENT);
     }
 
     @Override
     protected boolean getBoolean(String key, boolean defaultValue) {
-        return LineageSettings.System.getInt(getContext().getContentResolver(),
-                key, defaultValue ? 1 : 0) != 0;
+        return LineageSettings.System.getIntForUser(getContext().getContentResolver(),
+                key, defaultValue ? 1 : 0, UserHandle.USER_CURRENT) != 0;
     }
 }

AlphaDroid-Project/frameworks_base

packages/LineagePreferenceLib/java/lineageos/preference/LineageSecureSettingMainSwitchPreference.java
@@ -6,6 +6,7 @@
 package lineageos.preference;
 
 import android.content.Context;
+import android.os.UserHandle;
 import android.util.AttributeSet;
 
 import androidx.preference.PreferenceDataStore;
@@ -35,13 +36,13 @@ public class LineageSecureSettingMainSwitchPreference extends MainSwitchPreferen
     private class DataStore extends PreferenceDataStore {
         @Override
         public void putBoolean(String key, boolean value) {
-            Settings.Secure.putInt(getContext().getContentResolver(), key, value ? 1 : 0);
+            Settings.Secure.putIntForUser(getContext().getContentResolver(), key, value ? 1 : 0, UserHandle.USER_CURRENT);
         }
 
         @Override
         public boolean getBoolean(String key, boolean defaultValue) {
-            return Settings.Secure.getInt(getContext().getContentResolver(), key,
-                    defaultValue ? 1 : 0) != 0;
+            return Settings.Secure.getIntForUser(getContext().getContentResolver(), key,
+                    defaultValue ? 1 : 0, UserHandle.USER_CURRENT) != 0;
         }
     }
 }

packages/LineagePreferenceLib/java/lineageos/preference/LineageSecureSettingSwitchPreference.java
@@ -7,6 +7,7 @@
 package lineageos.preference;
 
 import android.content.Context;
+import android.os.UserHandle;
 import android.util.AttributeSet;
 
 import android.provider.Settings;
@@ -32,12 +33,12 @@ public class LineageSecureSettingSwitchPreference extends SelfRemovingSwitchPref
 
     @Override
     protected void putBoolean(String key, boolean value) {
-        Settings.Secure.putInt(getContext().getContentResolver(), key, value ? 1 : 0);
+        Settings.Secure.putIntForUser(getContext().getContentResolver(), key, value ? 1 : 0, UserHandle.USER_CURRENT);
     }
 
     @Override
     protected boolean getBoolean(String key, boolean defaultValue) {
-        return Settings.Secure.getInt(getContext().getContentResolver(),
-                key, defaultValue ? 1 : 0) != 0;
+        return Settings.Secure.getIntForUser(getContext().getContentResolver(),
+                key, defaultValue ? 1 : 0, UserHandle.USER_CURRENT) != 0;
     }
 }

packages/LineagePreferenceLib/java/lineageos/preference/LineageSystemSettingMainSwitchPreference.java
@@ -6,6 +6,7 @@
 package lineageos.preference;
 
 import android.content.Context;
+import android.os.UserHandle;
 import android.util.AttributeSet;
 
 import androidx.preference.PreferenceDataStore;
@@ -35,13 +36,13 @@ public class LineageSystemSettingMainSwitchPreference extends MainSwitchPreferen
     private class DataStore extends PreferenceDataStore {
         @Override
         public void putBoolean(String key, boolean value) {
-            Settings.System.putInt(getContext().getContentResolver(), key, value ? 1 : 0);
+            Settings.System.putIntForUser(getContext().getContentResolver(), key, value ? 1 : 0, UserHandle.USER_CURRENT);
         }
 
         @Override
         public boolean getBoolean(String key, boolean defaultValue) {
-            return Settings.System.getInt(getContext().getContentResolver(), key,
-                    defaultValue ? 1 : 0) != 0;
+            return Settings.System.getIntForUser(getContext().getContentResolver(), key,
+                    defaultValue ? 1 : 0, UserHandle.USER_CURRENT) != 0;
         }
     }
 }

packages/LineagePreferenceLib/java/lineageos/preference/LineageSystemSettingSwitchPreference.java
@@ -7,6 +7,7 @@
 package lineageos.preference;
 
 import android.content.Context;
+import android.os.UserHandle;
 import android.util.AttributeSet;
 
 import android.provider.Settings;
@@ -32,12 +33,12 @@ public class LineageSystemSettingSwitchPreference extends SelfRemovingSwitchPref
 
     @Override
     protected void putBoolean(String key, boolean value) {
-        Settings.System.putInt(getContext().getContentResolver(), key, value ? 1 : 0);
+        Settings.System.putIntForUser(getContext().getContentResolver(), key, value ? 1 : 0, UserHandle.USER_CURRENT);
     }
 
     @Override
     protected boolean getBoolean(String key, boolean defaultValue) {
-        return Settings.System.getInt(getContext().getContentResolver(),
-                key, defaultValue ? 1 : 0) != 0;
+        return Settings.System.getIntForUser(getContext().getContentResolver(),
+                key, defaultValue ? 1 : 0, UserHandle.USER_CURRENT) != 0;
     }
 }