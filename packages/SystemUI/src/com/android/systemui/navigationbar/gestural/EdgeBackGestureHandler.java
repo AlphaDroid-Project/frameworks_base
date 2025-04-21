alioth-aosp/android_frameworks_base

SystemUI: Disable edge gesture if navbar is disabled
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
@@ -79,6 +79,7 @@ import androidx.annotation.DimenRes;
 
 import com.android.internal.config.sysui.SystemUiDeviceConfigFlags;
 import com.android.internal.policy.GestureNavigationSettingsObserver;
+import com.android.internal.util.crdroid.Utils;
 import com.android.systemui.Dependency;
 import com.android.systemui.contextualeducation.GestureType;
 import com.android.systemui.dagger.qualifiers.Background;
@@ -147,6 +148,8 @@ public class EdgeBackGestureHandler implements PluginListener<NavigationEdgeBack
             "lineagesystem:" + LineageSettings.System.KEY_EDGE_LONG_SWIPE_ACTION;
     private static final String BACK_GESTURE_HEIGHT =
             "system:" + Settings.System.BACK_GESTURE_HEIGHT;
+    private static final String FORCE_SHOW_NAVBAR =
+            "lineagesystem:" + LineageSettings.System.FORCE_SHOW_NAVBAR;
 
     private static final int MAX_NUM_LOGGED_PREDICTIONS = 10;
     private static final int MAX_NUM_LOGGED_GESTURES = 10;
@@ -332,6 +335,7 @@ public class EdgeBackGestureHandler implements PluginListener<NavigationEdgeBack
 
     private boolean mIsBackGestureArrowEnabled;
     private boolean mIsEdgeHapticEnabled;
+    private boolean mNavbarVisible;
 
     private final NavigationEdgeBackPlugin.BackCallback mBackCallback =
             new NavigationEdgeBackPlugin.BackCallback() {
@@ -597,6 +601,9 @@ public class EdgeBackGestureHandler implements PluginListener<NavigationEdgeBack
                 && mButtonForcedVisibleCallback != null) {
             mButtonForcedVisibleCallback.accept(mIsButtonForcedVisible);
         }
+        mNavbarVisible = LineageSettings.System.getIntForUser(mContext.getContentResolver(),
+                LineageSettings.System.FORCE_SHOW_NAVBAR, Utils.hasNavbarByDefault(mContext) ? 1 : 0,
+                UserHandle.USER_CURRENT) != 0;
 
         final DisplayMetrics dm = res.getDisplayMetrics();
         final float defaultGestureHeight = res.getDimension(
@@ -678,6 +685,7 @@ public class EdgeBackGestureHandler implements PluginListener<NavigationEdgeBack
         mUserTracker.addCallback(mUserChangedCallback, mUiThreadContext.getExecutor());
         mTunerService.addTunable(this, KEY_EDGE_LONG_SWIPE_ACTION);
         mTunerService.addTunable(this, BACK_GESTURE_HEIGHT);
+        mTunerService.addTunable(this, FORCE_SHOW_NAVBAR);
     }
 
     /**
@@ -734,7 +742,7 @@ public class EdgeBackGestureHandler implements PluginListener<NavigationEdgeBack
 
             mIsGestureHandlingEnabled = mInGestureNavMode || (mUsingThreeButtonNav
                     && !mTrackpadsConnected.isEmpty());
-            boolean isEnabled = mIsAttached && mIsGestureHandlingEnabled;
+            boolean isEnabled = mIsAttached && mIsGestureHandlingEnabled && mNavbarVisible;
             if (isEnabled == mIsEnabled) {
                 return;
             }
@@ -864,6 +872,10 @@ public class EdgeBackGestureHandler implements PluginListener<NavigationEdgeBack
         } else if (BACK_GESTURE_HEIGHT.equals(key)) {
             mEdgeHeightSetting = TunerService.parseInteger(newValue, 0);
             updateEdgeHeightValue();
+        } else if (FORCE_SHOW_NAVBAR.equals(key)) {
+            mNavbarVisible =
+                TunerService.parseIntegerSwitch(newValue, Utils.hasNavbarByDefault(mContext));
+            updateIsEnabled();
         }
     }
 

AlphaDroid-Project/frameworks_base

packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
@@ -79,6 +79,7 @@ import androidx.annotation.DimenRes;
 
 import com.android.internal.config.sysui.SystemUiDeviceConfigFlags;
 import com.android.internal.policy.GestureNavigationSettingsObserver;
+import com.android.internal.util.alpha.Utils;
 import com.android.systemui.Dependency;
 import com.android.systemui.contextualeducation.GestureType;
 import com.android.systemui.dagger.qualifiers.Background;
@@ -147,6 +148,8 @@ public class EdgeBackGestureHandler implements PluginListener<NavigationEdgeBack
             "system:" + Settings.System.KEY_EDGE_LONG_SWIPE_ACTION;
     private static final String BACK_GESTURE_HEIGHT =
             "system:" + Settings.System.BACK_GESTURE_HEIGHT;
+    private static final String FORCE_SHOW_NAVBAR =
+            "system:" + Settings.System.FORCE_SHOW_NAVBAR;
 
     private static final int MAX_NUM_LOGGED_PREDICTIONS = 10;
     private static final int MAX_NUM_LOGGED_GESTURES = 10;
@@ -332,6 +335,7 @@ public class EdgeBackGestureHandler implements PluginListener<NavigationEdgeBack
 
     private boolean mIsBackGestureArrowEnabled;
     private boolean mIsEdgeHapticEnabled;
+    private boolean mNavbarVisible;
 
     private final NavigationEdgeBackPlugin.BackCallback mBackCallback =
             new NavigationEdgeBackPlugin.BackCallback() {
@@ -597,6 +601,9 @@ public class EdgeBackGestureHandler implements PluginListener<NavigationEdgeBack
                 && mButtonForcedVisibleCallback != null) {
             mButtonForcedVisibleCallback.accept(mIsButtonForcedVisible);
         }
+        mNavbarVisible = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.FORCE_SHOW_NAVBAR, Utils.hasNavbarByDefault(mContext) ? 1 : 0,
+                UserHandle.USER_CURRENT) != 0;
 
         final DisplayMetrics dm = res.getDisplayMetrics();
         final float defaultGestureHeight = res.getDimension(
@@ -678,6 +685,7 @@ public class EdgeBackGestureHandler implements PluginListener<NavigationEdgeBack
         mUserTracker.addCallback(mUserChangedCallback, mUiThreadContext.getExecutor());
         mTunerService.addTunable(this, KEY_EDGE_LONG_SWIPE_ACTION);
         mTunerService.addTunable(this, BACK_GESTURE_HEIGHT);
+        mTunerService.addTunable(this, FORCE_SHOW_NAVBAR);
     }
 
     /**
@@ -734,7 +742,7 @@ public class EdgeBackGestureHandler implements PluginListener<NavigationEdgeBack
 
             mIsGestureHandlingEnabled = mInGestureNavMode || (mUsingThreeButtonNav
                     && !mTrackpadsConnected.isEmpty());
-            boolean isEnabled = mIsAttached && mIsGestureHandlingEnabled;
+            boolean isEnabled = mIsAttached && mIsGestureHandlingEnabled && mNavbarVisible;
             if (isEnabled == mIsEnabled) {
                 return;
             }
@@ -864,6 +872,10 @@ public class EdgeBackGestureHandler implements PluginListener<NavigationEdgeBack
         } else if (BACK_GESTURE_HEIGHT.equals(key)) {
             mEdgeHeightSetting = TunerService.parseInteger(newValue, 0);
             updateEdgeHeightValue();
+        } else if (FORCE_SHOW_NAVBAR.equals(key)) {
+            mNavbarVisible =
+                TunerService.parseIntegerSwitch(newValue, Utils.hasNavbarByDefault(mContext));
+            updateIsEnabled();
         }
     }
 