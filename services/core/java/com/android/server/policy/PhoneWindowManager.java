8dc0187ca456aade2e9566ded985d5815f8beebb
LuK1337 <priv.luk@gmail.com>
2025-04-28 16:29:23 +0200

    fixup! fw/b: Implement hardware keys custom rebinding
    I'm honestly not sure what this was originally for...
    
    Test: Use home button double click to turn screen off and notice that it
          no longer goes home.
    Change-Id: If0b27444014b3103375f8cb6da618b1ddab305d0

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -2524,9 +2524,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     mPendingHomeKeyEvent = null;
                     mHandler.removeCallbacks(mHomeDoubleTapTimeoutRunnable);
                     performKeyAction(mHomeDoubleTapAction, event);
-                    if (mHomeDoubleTapAction != Action.SLEEP) {
-                        mHomeConsumed = true;
-                    }
+                    mHomeConsumed = true;
                 } else if (mDisplayId == DEFAULT_DISPLAY
                         && (mHomeLongPressAction == Action.APP_SWITCH
                             || mHomeDoubleTapAction == Action.APP_SWITCH)) {
@@ -2544,9 +2542,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     // it should pass this right invocation type.
                     performKeyAction(mHomeLongPressAction, event,
                             AssistUtils.INVOCATION_TYPE_HOME_BUTTON_LONG_PRESS);
-                    if (mHomeLongPressAction != Action.SLEEP) {
-                        mHomeConsumed = true;
-                    }
+                    mHomeConsumed = true;
                 }
             }
             return true;
