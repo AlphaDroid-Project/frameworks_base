474299ff976287ad0ca134243498d8f12cb2e250
Trogel <trogel@clink.de>
2025-05-02 10:54:24 +0000

    health: Update charging control if next alarm is changed
    Fixes: https://gitlab.com/LineageOS/issues/android/-/issues/8744
    Change-Id: I390f7c2110add51f776f56d9de1ab690721e2e21

--- services/core/java/org/lineageos/server/health/ChargingControlController.java
+++ services/core/java/org/lineageos/server/health/ChargingControlController.java
@@ -44,6 +44,7 @@ public class ChargingControlController extends LineageHealthFeature {
     private final ContentResolver mContentResolver;
     private ChargingControlNotification mChargingNotification;
     private LineageHealthBatteryBroadcastReceiver mBattReceiver;
+    private BroadcastReceiver mAlarmBroadcastReceiver;
 
     // Defaults
     private boolean mDefaultEnabled = false;
@@ -456,6 +457,26 @@ public class ChargingControlController extends LineageHealthFeature {
                 } else {
                     mChargingNotification.cancel();
                 }
+            }
+        }
+
+        if (mode == MODE_AUTO) {
+            if (mAlarmBroadcastReceiver == null) {
+                IntentFilter alarmChangedFilter = new IntentFilter(
+                        android.app.AlarmManager.ACTION_NEXT_ALARM_CLOCK_CHANGED);
+                mAlarmBroadcastReceiver = new BroadcastReceiver() {
+                    @Override
+                    public void onReceive(Context context, Intent intent) {
+                        Log.i(TAG, "Alarm changed, update charge times");
+                        updateChargeControl();
+                    }
+                };
+                mContext.registerReceiver(mAlarmBroadcastReceiver, alarmChangedFilter);
+            }
+        } else {
+            if (mAlarmBroadcastReceiver != null) {
+                mContext.unregisterReceiver(mAlarmBroadcastReceiver);
+                mAlarmBroadcastReceiver = null;
             }
         }
     }
