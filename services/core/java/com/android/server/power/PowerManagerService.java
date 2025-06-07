tavukkdoner/android_frameworks_base

fixup! PowerManager: Add proximity check on wake
Issue: calyxos#3134
Change-Id: Icf78eebe4749b417484cdd16a7b97f3c6e771ca5
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

services/core/java/com/android/server/power/PowerManagerService.java
@@ -1702,9 +1702,9 @@ public final class PowerManagerService extends SystemService
             mSystemProperties.set(SYSTEM_PROPERTY_RETAIL_DEMO_ENABLED, retailDemoValue);
         }
 
-        mProximityWakeEnabled = LineageSettings.System.getInt(resolver,
+        mProximityWakeEnabled = LineageSettings.System.getIntForUser(resolver,
                 LineageSettings.System.PROXIMITY_ON_WAKE,
-                mProximityWakeEnabledByDefaultConfig ? 1 : 0) == 1;
+                mProximityWakeEnabledByDefaultConfig ? 1 : 0, UserHandle.USER_CURRENT) == 1;
 
         mButtonTimeout = LineageSettings.Secure.getIntForUser(resolver,
                 LineageSettings.Secure.BUTTON_BACKLIGHT_TIMEOUT,

AlphaDroid-Project/frameworks_base

services/core/java/com/android/server/power/PowerManagerService.java
@@ -1702,9 +1702,9 @@ public final class PowerManagerService extends SystemService
             mSystemProperties.set(SYSTEM_PROPERTY_RETAIL_DEMO_ENABLED, retailDemoValue);
         }
 
-        mProximityWakeEnabled = Settings.System.getInt(resolver,
+        mProximityWakeEnabled = Settings.System.getIntForUser(resolver,
                 Settings.System.PROXIMITY_ON_WAKE,
-                mProximityWakeEnabledByDefaultConfig ? 1 : 0) == 1;
+                mProximityWakeEnabledByDefaultConfig ? 1 : 0, UserHandle.USER_CURRENT) == 1;
 
         mButtonTimeout = Settings.Secure.getIntForUser(resolver,
                 Settings.Secure.BUTTON_BACKLIGHT_TIMEOUT,