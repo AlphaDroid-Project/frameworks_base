c6b82f6cef22b7d22429500f403e5284954d87ea
Pranav Vashi <neobuddy89@gmail.com>
2025-10-09 19:47:50 +0530

    SystemUI: Add toggle for translucent notifications on lockscreen
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/shade/QuickSettingsControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/shade/QuickSettingsControllerImpl.java
@@ -140,6 +140,8 @@ public class QuickSettingsControllerImpl implements QuickSettingsController, Dum
             "lineagesystem:" + LineageSettings.System.STATUS_BAR_QUICK_QS_PULLDOWN;
     private static final String NOTIFICATION_ROW_TRANSPARENCY =
             Settings.Secure.NOTIFICATION_ROW_TRANSPARENCY;
+    private static final String NOTIFICATION_ROW_TRANSPARENCY_LOCKSCREEN =
+            Settings.Secure.NOTIFICATION_ROW_TRANSPARENCY_LOCKSCREEN;
 
     private QS mQs;
     private final Lazy<NotificationPanelViewController> mPanelViewControllerLazy;
@@ -2382,6 +2384,7 @@ public class QuickSettingsControllerImpl implements QuickSettingsController, Dum
             mQs.setScrollListener(mQsScrollListener);
             mTunerService.addTunable(this, STATUS_BAR_QUICK_QS_PULLDOWN);
             mTunerService.addTunable(this, NOTIFICATION_ROW_TRANSPARENCY);
+            mTunerService.addTunable(this, NOTIFICATION_ROW_TRANSPARENCY_LOCKSCREEN);
             updateExpansion();
         }
 
@@ -2411,7 +2414,8 @@ public class QuickSettingsControllerImpl implements QuickSettingsController, Dum
         public void onTuningChanged(String key, String newValue) {
             if (STATUS_BAR_QUICK_QS_PULLDOWN.equals(key)) {
                 mOneFingerQuickSettingsIntercept = TunerService.parseInteger(newValue, 0);
-            } else if (NOTIFICATION_ROW_TRANSPARENCY.equals(key)) {
+            } else if (NOTIFICATION_ROW_TRANSPARENCY.equals(key) ||
+                    NOTIFICATION_ROW_TRANSPARENCY_LOCKSCREEN.equals(key)) {
                 updateTransparencyIfNeeded();
             }
         }
