2573e332fbb1f9b5d691af021620d504b5399b2e
elpaablo <donopwhile1@gmail.com>
2026-02-12 12:16:36 +0000

    Revert "androidfw: Add Lineage res to default assets"
    This reverts commit ca4d3db57d3261d663b622b299a613c2630f0209.

--- libs/androidfw/AssetManager.cpp
+++ libs/androidfw/AssetManager.cpp
@@ -63,7 +63,6 @@ static const bool kIsDebug = false;
 static const char* kAssetsRoot = "assets";
 static const char* kAppZipName = NULL; //"classes.jar";
 static const char* kSystemAssets = "framework/framework-res.apk";
-static const char* kLineageAssets = "framework/org.lineageos.platform-res.apk";
 static const char* kResourceCache = "resource-cache";
 
 static const char* kExcludeExtension = ".EXCLUDE";
@@ -368,18 +367,10 @@ bool AssetManager::addDefaultAssets()
     const char* root = getenv("ANDROID_ROOT");
     LOG_ALWAYS_FATAL_IF(root == NULL, "ANDROID_ROOT not set");
 
-    bool success = true;
-    {
-       String8 path(root);
-       appendPath(path, kSystemAssets);
-       success &= addAssetPath(path, NULL, false /* appAsLib */, true /* isSystemAsset */);
-    }
-    {
-       String8 path(root);
-       appendPath(path, kLineageAssets);
-       success &= addAssetPath(path, NULL, false /* appAsLib */, true /* isSystemAsset */);
-    }
-    return success;
+    String8 path(root);
+    appendPath(path, kSystemAssets);
+
+    return addAssetPath(path, NULL, false /* appAsLib */, true /* isSystemAsset */);
 }
 
 int32_t AssetManager::nextAssetPath(const int32_t cookie) const
