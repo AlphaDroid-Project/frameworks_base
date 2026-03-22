1ab513a65d76170fe73456b9daad7e1d71fcfb34
elpaablo <donopwhile1@gmail.com>
2026-03-22 11:13:13 +0000

    Revert "SystemUI: Add custom gradient start/end color support"
    This reverts commit 4b06b49cd9b877bbfce3011f9b822fafb0114730.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7528,24 +7528,6 @@ public final class Settings {
         public static final String VOLUME_SLIDER_GRADIENT = "volume_slider_gradient";
 
         /**
-         * Gradient color mode
-         * @hide
-         */
-        public static final String CUSTOM_GRADIENT_COLOR_MODE = "custom_gradient_color_mode";
-
-        /**
-         * Gradient start color
-         * @hide
-         */
-        public static final String CUSTOM_GRADIENT_START_COLOR = "custom_gradient_start_color";
-
-        /**
-         * Gradient end color
-         * @hide
-         */
-        public static final String CUSTOM_GRADIENT_END_COLOR = "custom_gradient_end_color";
-
-        /**
          * @hide
          */
         public static final String ONGOING_ACTION_CHIP = "ongoing_action_chip";
